c588695b207fa6f9f713f8e31347e2d02cb2b34e
ikaverin <kaverin1990@gmail.com>
2022-05-15 23:43:55 +0300

    refactoring header

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ export default () => {
 
     const cookie = 'coooookie om nom nom';
 
-    if(!localStorage.getItem('wasHere')) {
-        localStorage.setItem('wasHere', cookie)
+    if(!sessionStorage.getItem('wasHere')) {
+        sessionStorage.setItem('wasHere', cookie)
     } else {
         const animated = document.querySelectorAll('.animated');
         animated.forEach((node, index) => node.classList.remove(`animation-${index}`));
