5db6f389bdecb78cc397eec3dda5c1970999dab4
ikaverin <kaverin1990@gmail.com>
2022-05-17 18:51:51 +0300

    add autoslider

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import app from "./scripts/app.js";
 import animate from "./scripts/animations";
 
+
+
 app();
 animate();
--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -1,10 +1,165 @@
 export default () => {
-  const contacts = document.querySelector(".header-contacts");
-  const showContactsButton = document.querySelector(
-    ".heading-contact-container button"
-  );
 
-  showContactsButton.addEventListener("click", () => {
-    contacts.classList.toggle("active");
-  });
+    const Cards = {
+
+        init: () => {
+            Cards.triggerCardChange();
+            Cards.directlyClickOnCards();
+        },
+    
+        //  Change the active Card on directly clicking on it
+        // - - - - - - - - - - - - - - - - - - - - - - - - - -
+    
+        directlyClickOnCards: () => {
+            let cards = conf.qSA('.card');
+            if(cards.length){
+                cards.forEach(function(item){
+                   item.onclick = () => {
+                       if(!item.classList.contains('active')){
+                           // search the active card
+                           for(let i = 0; i < cards.length; i++){
+                               if(cards[i].classList.contains('active')){
+                                   let dataCard = cards[i];
+                                   dataCard.classList.add('inactive');
+                                   dataCard.classList.remove('active');
+                                   break;
+                               }
+                           }
+    
+                           conf.qS('.cards-wrapper').prepend(item);
+                           item.classList.remove('inactive');
+                           item.classList.add('active');
+                       }
+                   }
+                });
+            }
+        },
+    
+        //  Change the active Card
+        // - - - - - - - - - - - - - - - - - - - - - - - - - -
+    
+        triggerCardChange: () => {
+    
+            let arrow = conf.qS('.slide-button'),
+                cards = conf.qSA('.card');
+    
+            if(arrow){
+                arrow.onclick = () => {
+                    if(cards.length) {
+                        for(let i = 0; i < cards.length; i++){
+                            if(cards[i].classList.contains('active')){
+                                let dataCard = cards[i];
+                                getNextCard(dataCard);
+                                break;
+                            }
+                        }
+                    }
+    
+                    function getNextCard(prevCard){
+    
+                        for(let i = 0; i < cards.length; i++){
+                            let dataCard = parseInt(prevCard.getAttribute('data-card'), 10),
+                                nextCard = parseInt(cards[i].getAttribute('data-card'), 10);
+    
+                            if((dataCard + 1) === nextCard){
+                                prevCard.classList.add('inactive');
+                                prevCard.classList.remove('active');
+                                conf.qS('.cards-wrapper').prepend(cards[i]);
+                                cards[i].classList.remove('inactive');
+                                cards[i].classList.add('active');
+                                break;
+                            }else if((dataCard + 1) >= cards.length){
+                                prevCard.classList.add('inactive');
+                                prevCard.classList.remove('active');
+                                conf.qS('.cards-wrapper').prepend(cards[i]);
+                                cards[0].classList.remove('inactive');
+                                cards[0].classList.add('active');
+                                break;
+                            }
+                        }
+                    }
+                };
+            }
+        }
+    };
+
+    
+    
+    //  Config Functions
+    // - - - - - - - - - - - - - - - - - - - - - - - - - -
+    
+    const conf = {
+        qS: (selector) => {
+            return document.querySelector(selector);
+        },
+        qSA: (selector) => {
+            return document.querySelectorAll(selector);
+        },
+        CqS: (container, selector) => {
+            return container.querySelector(selector);
+        },
+        InfoBox: () => {
+            let toggle = conf.qS('.infobox-container .infobox-toggle'),
+                detail = conf.qS('.infobox-container .infobox-detail-container');
+    
+            if(toggle){
+                toggle.onclick = (e) => {
+                    e.preventDefault();
+                    detail.classList.toggle('active');
+                }
+            }
+        }
+    };
+
+    const autoSlide = () => {
+        let cards = conf.qSA('.card');
+        if(cards.length) {
+            for(let i = 0; i < cards.length; i++){
+                if(cards[i].classList.contains('active')){
+                    let dataCard = cards[i];
+                    getNextCard(dataCard);
+                    break;
+                }
+            }
+            function getNextCard(prevCard){
+    
+                for(let i = 0; i < cards.length; i++){
+                    let dataCard = parseInt(prevCard.getAttribute('data-card'), 10),
+                        nextCard = parseInt(cards[i].getAttribute('data-card'), 10);
+
+                    if((dataCard + 1) === nextCard){
+                        prevCard.classList.add('inactive');
+                        prevCard.classList.remove('active');
+                        conf.qS('.cards-wrapper').prepend(cards[i]);
+                        cards[i].classList.remove('inactive');
+                        cards[i].classList.add('active');
+                        break;
+                    }else if((dataCard + 1) >= cards.length){
+                        prevCard.classList.add('inactive');
+                        prevCard.classList.remove('active');
+                        conf.qS('.cards-wrapper').prepend(cards[i]);
+                        cards[0].classList.remove('inactive');
+                        cards[0].classList.add('active');
+                        break;
+                    }
+                }
+            }
+
+        }}
+
+
+
+    document.addEventListener('DOMContentLoaded', function () {
+
+        'use strict';
+        
+        Cards.init();
+        conf.InfoBox();
+        console.log(document.querySelector('.slide-button'))
+        setInterval(autoSlide, 2500)
+
+    });
+       
+
 };
+ // thanks to koby.dev
