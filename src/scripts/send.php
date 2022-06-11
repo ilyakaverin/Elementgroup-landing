f8d1efdb630db381fe82043f9f921d7db565d3df
ikaverin <kaverin1990@gmail.com>
2022-06-11 23:52:56 +0300

    some text fixes, changed chatid in send.php

--- src/scripts/send.php
+++ src/scripts/send.php
@@ -1,11 +1,11 @@
 <?php
-$apiToken = "5308589426:AAHiSuLvIFp-bccMLUQmpmnVoQQJq2x3sK8";
+$apiToken = "5475176541:AAGD2wAV-1EUFWBTcx9LK9JM9edfxqGwLVY";
 $_POST = json_decode(file_get_contents("php://input"),true);
     if(isset($_POST["data"]))
     {
         
         $data = [
-            "chat_id" => "41850257", 
+            "chat_id" => "571951479", 
             "text" => $_POST['data']
         ];
         $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );    
