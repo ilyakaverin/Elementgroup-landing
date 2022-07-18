81207ea82f87457a065ddcc048658d06f750f185
ikaverin <kaverin1990@gmail.com>
2022-07-18 21:46:27 +0300

    refactor slider

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,76 +1,38 @@
+import $ from 'jquery';
+
 export default () => {
-    const slider = document.querySelector(".slider");
-const sliderContent = document.querySelector(".item-wrapper");
-const sliderItems = sliderContent.querySelectorAll(".pic");
-const itemCount = sliderItems.length;
-const prevBtn = document.querySelector("#previous");
-const nextBtn = document.querySelector("#next");
-const indicatorWrapper = document.querySelector(".indicator-wrapper");
-
-let currentIndex = 0;
-
-const handleButtonClick = (direction) => {
-  if (direction === "left") {
-    currentIndex++;
-
-    if (currentIndex > itemCount - 1) {
-      currentIndex = 0;
+    var index = document.getElementsByClassName('image');
+    var length = index.length;
+    var slide = 0;
+    
+    // we run through the collection of elements and assign display: none to them;
+    for (var i = 1; i < index.length; i++) {
+        index[i].style.display = 'none';
     }
-  } else {
-    currentIndex--;
-
-    if (currentIndex < 0) {
-      currentIndex = itemCount - 1;
+    // button to the right;
+    document.getElementById('right').onclick = function Right () {
+        if(slide == length-1){
+            index[0].style.display = "block";
+            index[slide].style.display = "none";
+            slide = 0;
+        } else {
+            index[slide+1].style.display = "block";
+            index[slide].style.display = "none";
+            slide++;
+        }
     }
-  }
-
-  moveSlide();
-  styleIndicator();
-};
-
-const handleIndicatorClick = (e) => {
-  if (!e.target.classList.contains("indicator")) return;
-
-  currentIndex = e.target.dataset.index;
-
-  moveSlide();
-  styleIndicator();
-};
-
-const initIndicators = () => {
-  for (let i = 0; i < itemCount; i++) {
-    const indicator = document.createElement("div");
-    indicator.classList.add("indicator");
-    indicator.dataset.index = i;
-    indicatorWrapper.appendChild(indicator);
-  }
-
-  styleIndicator();
-};
-
-const styleIndicator = () => {
-  const indicators = indicatorWrapper.querySelectorAll(".indicator");
-  indicators.forEach((indicator) => indicator.classList.remove("current"));
-  indicators[currentIndex].classList.add("current");
-};
-
-const moveSlide = () => {
-  const sliderWidth = slider.clientWidth;
-  sliderContent.style.transform = `translateX(-${
-    currentIndex * sliderWidth
-  }px)`;
-};
-
-const addEventListeners = () => {
-  prevBtn.addEventListener("click", () => handleButtonClick("left"));
-  nextBtn.addEventListener("click", () => handleButtonClick("right"));
-  indicatorWrapper.addEventListener("click", handleIndicatorClick);
-};
-
-const init = () => {
-  addEventListeners();
-  initIndicators();
-};
-
-init();
+    // button to the left;
+    document.getElementById('left').onclick = function Left () {
+        if(slide <= 0) {
+            index[slide].style.display = "none";
+            slide = length;
+            slide--;
+            index[slide].style.display = "block";
+        } else {
+            index[slide].style.display = "none";
+            slide--;
+            index[slide].style.display = "block";
+        }
+    }
+    
 }
