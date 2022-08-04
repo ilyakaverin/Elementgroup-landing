d11ed16bfde4d986b8cd9a4b58abf71829d28f53
ikaverin <kaverin1990@gmail.com>
2022-08-04 23:36:34 +0300

    refactor slider

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,38 +1,33 @@
-import $ from 'jquery';
-
 export default () => {
-    var index = document.getElementsByClassName('image');
-    var length = index.length;
-    var slide = 0;
+    const sliderContainer = document.querySelector('.slider-container')
+    const slideRight = document.querySelector('.right-slide')
+    const slideLeft = document.querySelector('.left-slide')
+    const upButton = document.querySelector('.up-button')
+    const downButton = document.querySelector('.down-button')
+    const slidesLength = slideRight.querySelectorAll('div').length
     
-    // we run through the collection of elements and assign display: none to them;
-    for (var i = 1; i < index.length; i++) {
-        index[i].style.display = 'none';
-    }
-    // button to the right;
-    document.getElementById('right').onclick = function Right () {
-        if(slide == length-1){
-            index[0].style.display = "block";
-            index[slide].style.display = "none";
-            slide = 0;
-        } else {
-            index[slide+1].style.display = "block";
-            index[slide].style.display = "none";
-            slide++;
-        }
-    }
-    // button to the left;
-    document.getElementById('left').onclick = function Left () {
-        if(slide <= 0) {
-            index[slide].style.display = "none";
-            slide = length;
-            slide--;
-            index[slide].style.display = "block";
-        } else {
-            index[slide].style.display = "none";
-            slide--;
-            index[slide].style.display = "block";
-        }
-    }
+    let activeSlideIndex = 0
     
+    slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+    
+    upButton.addEventListener('click', () => changeSlide('up'))
+    downButton.addEventListener('click', () => changeSlide('down'))
+    
+    const changeSlide = (direction) => {
+        const sliderHeight = sliderContainer.clientHeight
+        if(direction === 'up') {
+            activeSlideIndex++
+            if(activeSlideIndex > slidesLength - 1) {
+                activeSlideIndex = 0
+            }
+        } else if(direction === 'down') {
+            activeSlideIndex--
+            if(activeSlideIndex < 0) {
+                activeSlideIndex = slidesLength - 1
+            }
+        }
+    
+        slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+        slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
+    }
 }
