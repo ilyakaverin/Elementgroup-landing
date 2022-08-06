44f8fc149872e462befdac4e336828db4d90b6d0
ikaverin <kaverin1990@gmail.com>
2022-08-06 17:55:35 +0300

    refactor slideshow

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,33 +1,27 @@
 export default () => {
-    const sliderContainer = document.querySelector('.slider-container')
-    const slideRight = document.querySelector('.right-slide')
-    const slideLeft = document.querySelector('.left-slide')
-    const upButton = document.querySelector('.up-button')
-    const downButton = document.querySelector('.down-button')
-    const slidesLength = slideRight.querySelectorAll('div').length
-    
-    let activeSlideIndex = 0
-    
-    slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
-    
-    upButton.addEventListener('click', () => changeSlide('up'))
-    downButton.addEventListener('click', () => changeSlide('down'))
-    
-    const changeSlide = (direction) => {
-        const sliderHeight = sliderContainer.clientHeight
-        if(direction === 'up') {
-            activeSlideIndex++
-            if(activeSlideIndex > slidesLength - 1) {
-                activeSlideIndex = 0
-            }
-        } else if(direction === 'down') {
-            activeSlideIndex--
-            if(activeSlideIndex < 0) {
-                activeSlideIndex = slidesLength - 1
-            }
-        }
-    
-        slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
-        slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
-    }
+ const pictures = document.querySelectorAll('.main-content-gallery-inner-container .gallery .pic');
+ const modal = document.querySelector('.modal__root');
+ const modalInner = document.querySelector('.modal_inner');
+ const next = document.querySelector('.gallery__mobile_next');
+ const prev = document.querySelector('.gallery__mobile_prev');
+ 
+ pictures.forEach(picture => {
+     picture.addEventListener('click', () => {
+         const image = picture.querySelector('img');
+         const url = image.src;
+         const img = document.createElement('img');
+         img.src = url;
+         modalInner.innerHTML = null
+         modalInner.append(img);
+         modal.classList.add('open');
+     })
+ })
+
+ next.addEventListener('click', () => {
+     for(const pic of pictures) {
+         pic.classList.add('showpic')
+     }
+ })
+
+ 
 }
